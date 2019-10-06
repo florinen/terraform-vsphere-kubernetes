1a46dd3d675b0e99464f6e59e8b1053808d44b42
florinen <florinen>
2019-10-06 18:29:08 -0500

    changed  controller ip count

--- output.tf
+++ output.tf
@@ -5,7 +5,7 @@ output "node_ips" {
    value = "${vsphere_virtual_machine.kubernetes_nodes.*.default_ip_address}"
 }
 output "controller_vm-moref" {
-   value = "${vsphere_virtual_machine.kubernetes_controller.moid}"
+   value = "${vsphere_virtual_machine.kubernetes_controller.*.moid}"
 }
 output "node_vm-morefs" {
    value = "${vsphere_virtual_machine.kubernetes_nodes.*.moid}"
