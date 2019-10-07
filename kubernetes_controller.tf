ca91405412cd0933d728d6f53bf06cdd63c327fe
florinen <florinen>
2019-10-06 19:34:15 -0500

    changed back tp program cmd

--- kubernetes_controller.tf
+++ kubernetes_controller.tf
@@ -170,7 +170,7 @@ resource "vsphere_virtual_machine" "kubernetes_controller" {
 }
 
 data "external" "kubeadm-init-info" {
-  command = ["sh", "${path.module}/scripts/kubeadm_init_info.sh"]
+  program = ["bash", "${path.module}/scripts/kubeadm_init_info.sh"]
   query = {
     ip_address  = "${vsphere_virtual_machine.kubernetes_controller.*.default_ip_address}"
     private_key = "${var.virtual_machine_kubernetes_controller["private_key"]}"
